a62debada2659081301ba03f7bb383373df68987
rulasg <rulasg@outlook.com>
2023-10-09 15:11:47 +0200

    remove PS warnings

--- RepoHelperTest/private/parameter.test.helper.ps1
+++ RepoHelperTest/private/parameter.test.helper.ps1
@@ -1,16 +1,18 @@
+# Variables used to the written output of the cmdlets
 
-# Variables used to the written output of the cmdlets 
-
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Scope='Script')]
 $WarningParameters = @{
-    WarningAction = 'SilentlyContinue' 
+    WarningAction = 'SilentlyContinue'
     WarningVariable = 'warningVar'
 }
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Scope='Script')]
 $InfoParameters = @{
     InformationAction = 'SilentlyContinue'
     InformationVariable = 'infoVar'
 }
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Scope='Script')]
 $ErrorParameters = @{
     ErrorAction = 'Silently'
     ErrorVariable = 'errorVar'
--- public/repo-create.ps1
+++ public/repo-create.ps1
@@ -31,26 +31,26 @@ function New-RepoFromModule {
         } else {
             $repoPath = $Path
         }
-        
+
         $repoPath = $repoPath | Convert-Path
-        
+
         $manifestPath = $repoPath | Join-Path -ChildPath *.psd1 | Get-Item
-        
+
         if($manifestPath.Count -eq 0 ){
             throw "No module found. Please move to a folder with a powershell module on it to import to GitHub"
         }
-        
+
         if ($manifestPath.Count -ne 1) {
             throw "More than one module manifest found."
         }
-        
+
         $description = (Import-PowerShellDataFile -Path $manifestPath).Description
-        
+
         $command = 'gh repo create --public -d {description} -s {path} --disable-wiki --push'
-        
+
         $command = $command -replace "{description}", "`"$($description)`""
         $command = $command -replace "{path}", "`"$($repoPath)`""
-        
+
         if ($PSCmdlet.ShouldProcess("Repo", $command)) {
 
             Invoke-Expression $command
