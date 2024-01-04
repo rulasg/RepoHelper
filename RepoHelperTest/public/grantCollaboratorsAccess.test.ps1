07b097225ef10e90f07190d72e533f9ce6f353f6
rulasg <rulasg@outlook.com>
2024-01-04 09:52:43 +0100

    Get Repo Access Fixes #19

--- RepoHelperTest/public/grantCollaboratorsAccess.test.ps1
+++ RepoHelperTest/public/grantCollaboratorsAccess.test.ps1
@@ -22,3 +22,30 @@ function RepoHelperTest_GrantRepoAccess_fail_wrong_user_repo_owner{
 
     Assert-AreEqual -expected 'Not Found' -Presented $result.message
 }
+
+function RepoHelperTest_GetRepoAccessAll_SUCCESS{
+
+    $GetAccessAllSuccess = $PSScriptRoot | Join-Path -ChildPath 'testData' -AdditionalChildPath 'getAccessAllSuccess.json'
+
+    $owner = 'solidifycustomers' ; $repo = 'bit21' 
+
+    Set-InvokeCommandAlias -Alias 'gh api repos/solidifycustomers/bit21/collaborators' -Command "Get-Content -Path $(($GetAccessAllSuccess | Get-Item).FullName)"
+
+    $result = Get-RepoAccessAll -owner $owner -repo $repo
+
+    Assert-AreEqual -Expected $result.raulgeu -Presented 'write'
+    Assert-AreEqual -Expected $result.rulasg -Presented 'admin'
+}
+
+function RepoHelperTest_GetRepoAccess_Success{
+
+    $GetAccessSuccess = $PSScriptRoot | Join-Path -ChildPath 'testData' -AdditionalChildPath 'getAccessSuccess.json'
+
+    $owner = 'solidifycustomers' ; $repo = 'bit21' ; $user = 'raulgeu'
+
+    Set-InvokeCommandAlias -Alias 'gh api repos/solidifycustomers/bit21/collaborators/raulgeu/permission' -Command "Get-Content -Path $(($GetAccessSuccess | Get-Item).FullName)"
+
+    $result = Get-RepoAccess -Owner $owner -Repo $repo -User $user
+
+    Assert-AreEqual -Expected 'write' -Presented $result
+}
