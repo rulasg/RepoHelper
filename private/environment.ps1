3ba8319433818a70cfc20a4139dfb4ad9aec3079
rulasg <rulasg@outlook.com>
2024-01-09 10:46:56 +0100

    environment.ps1

---
+++ private/environment.ps1
@@ -0,0 +1,34 @@
+
+Set-InvokeCommandAlias -Alias 'GetRepoRemoteUrl' -Command 'git remote get-url origin 2>$null'
+
+function Get-Environment{
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        [Parameter()] [string]$Owner,
+        [Parameter()] [string]$Repo
+    )
+
+    $url = Invoke-MyCommand -Command GetRepoRemoteUrl
+    if($null -ne $url){
+
+        $remoteRepo = $url | Split-Path -Leafbase
+        $remoteOwner = $url | Split-Path -Parent | Split-Path -Leafbase
+    }
+
+    # Default owner
+    if([string]::IsNullOrWhiteSpace($Owner)){
+        $Owner = $remoteOwner
+    }
+
+    # Default owner
+    if([string]::IsNullOrWhiteSpace($Repo)){
+        $Repo = $remoteRepo
+    }
+
+    return [PSCustomObject]@{
+        Repo = $Repo
+        Owner = $Owner
+        RepoWithOwner = "$Owner/$Repo"
+    }
+}
